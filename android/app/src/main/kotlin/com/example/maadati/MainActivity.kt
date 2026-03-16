1a31a08f702a69c32591ed4eeefe9f160f8ce935
khaili-eng <khalildarwish2580@gmail.com>
2026-03-17 00:17:52 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/maadati/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.maadati
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/constants/app_color/app_color.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColor{
+  //background
+  static const Color color1 =Color(0xFFFFFBF1);
+  //light card, secondary background
+  static const Color color2 = Color(0xFFFFF2D0);
+  //soft Accent
+  static const Color color3 = Color(0xFFFFB2B2);
+  //primary color
+  static const Color color4 =Color(0xFFE36A6A);
+}
